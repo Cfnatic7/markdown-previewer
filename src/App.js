29e79ee23c7eddc480479020e94cfa37ed25880e
Cfnatic7 <michalpasicki1@gmail.com>
2022-02-25 15:27:00 +0100

    Added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import {Header} from './components/Header'
 
 function App() {
   return (
-      
+    <Header/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import { store } from './app/store';
+import { store } from './reduxPart/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
